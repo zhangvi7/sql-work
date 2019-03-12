5828ff46070431201aca04de7a91ab2bad23aac6
Victor <victorzhang1997@hotmail.com>
2019-03-12 14:22:32 -0400

    d

--- q2.sql
+++ q2.sql
@@ -34,9 +34,17 @@ select w.country_id, w.party_id, count(*) as party_wins
 from winning_party w
 group by w.party_id, w.country_id;
 
+select w.country_id, w.party_id, count(*) as party_wins
+from winning_party w
+group by w.party_id, w.country_id;
+
 -- 3) find country avg win
 DROP VIEW IF EXISTS avg_wins_country CASCADE;
 create view avg_wins_country as
+select w.country_id, (sum(w.party_wins) / count(w.party_id)) as country_avg_win
+from wins_per_party w
+group by w.country_id;
+
 select w.country_id, (sum(w.party_wins) / count(w.party_id)) as country_avg_win
 from wins_per_party w
 group by w.country_id;
