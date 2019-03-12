e39b6a93300e46f5115d26bc70fb8d143a0c0fae
Victor <victorzhang1997@hotmail.com>
2019-03-12 16:09:00 -0400

    x

--- q2.sql
+++ q2.sql
@@ -28,52 +28,47 @@ from election_result e, winning_partys w, party p
 where e.election_id = w.election_id and e.votes = w.max_votes and 
       e.party_id = p.id;
 --2) Using all winning parties in election, find number of wins per party
--- DROP VIEW IF EXISTS wins_per_party_no_country CASCADE;
--- create view wins_per_party_no_country as
--- select w.party_id, count(p.country_id) as party_wins
--- from winning_party w right join party p on w.party_id = p.id
--- group by w.party_id; 
+DROP VIEW IF EXISTS wins_per_party_no_country CASCADE;
+create view wins_per_party_no_country as
+select w.party_id, count(p.country_id) as party_wins
+from winning_party w right join party p on w.party_id = p.id
+group by w.party_id; 
 
--- DROP VIEW IF EXISTS wins_per_party CASCADE;
--- CREATE VIEW wins_per_party AS
--- SELECT w.party_id, p.country_id, w.party_wins
--- FROM wins_per_party_no_country w LEFT JOIN party p ON p.id= w.party_id;
-
--- DROP VIEW IF EXISTS wins_per_party CASCADE;
--- CREATE VIEW wins_per_party AS
-SELECT num.party_id, party.country_id, num.num_of_winning
-FROM(SELECT winning_party.party_id , count(party.country_id) AS num_of_winning 
-FROM winning_party  RIGHT JOIN party ON winner_party.party_id = party.id GROUP BY winning_party.party_id) num  LEFT JOIN party ON party.id= num.party_id;
+DROP VIEW IF EXISTS wins_per_party CASCADE;
+CREATE VIEW wins_per_party AS
+SELECT w.party_id, p.country_id, w.party_wins
+FROM wins_per_party_no_country w LEFT JOIN party p ON p.id= w.party_id;
 
 -- 3)average number of winning elections of parties of the same country
 
--- DROP VIEW IF EXISTS avg_wins_country CASCADE;
--- create view avg_wins_country as
--- select p.country_id, (sum(w.party_wins) / count(w.party_id)) as country_avg_win
--- from wins_per_party w right join party p on w.party_id = p.id
--- group by p.country_id;
+DROP VIEW IF EXISTS avg_wins_country CASCADE;
+create view avg_wins_country as
+select p.country_id, (sum(w.party_wins) / count(w.party_id)) as country_avg_win
+from wins_per_party w right join party p on w.party_id = p.id
+group by p.country_id;
 
--- --4) Find parties that won more than 3 x average win per country 
--- DROP VIEW IF EXISTS won_more_3x CASCADE;
--- create view won_more_3x as
+--4) Find parties that won more than 3 x average win per country 
+DROP VIEW IF EXISTS won_more_3x CASCADE;
+create view won_more_3x as
+select w.country_id, w.party_wins, w.party_id
+from wins_per_party w join avg_wins_country a on
+w.country_id = a.country_id 
+where w.party_wins > (3 * a.country_avg_win);
+
+create view won_more_3x as
+select w.country_id, w.party_wins, w.party_id
+from wins_per_party w join avg_wins_country a on
+w.country_id = a.country_id 
+where w.party_wins > (3 * a.country_avg_win);
+
 -- select w.country_id, w.party_wins, w.party_id
--- from wins_per_party w join avg_wins_country a on
--- w.country_id = a.country_id 
--- where 3 * (a.country_avg_win) < w.party_wins;
-
--- select w.country_id, w.party_wins, w.party_id
--- from wins_per_party w join avg_wins_country a on
--- w.country_id = a.country_id 
--- where 3 * (a.country_avg_win) < w.party_wins;
-
--- -- select w.country_id, w.party_wins, w.party_id
--- -- from wins_per_party w, avg_wins_country a
--- -- where w.country_id = a.country_id and w.party_wins > (3 * a.country_avg_win);
+-- from wins_per_party w, avg_wins_country a
+-- where w.country_id = a.country_id and w.party_wins > (3 * a.country_avg_win);
      
--- -- --5) Find most recently won election id/year for each party
--- -- DROP VIEW IF EXISTS most_recent_date CASCADE;
--- -- create view most_recent_date as
--- -- select w.party_id, max(e.e_date) as mostRecentlyWonElectionDate, e.id as mostRecentlyWonElectionId
+-- --5) Find most recently won election id/year for each party
+-- DROP VIEW IF EXISTS most_recent_date CASCADE;
+-- create view most_recent_date as
+-- select w.party_id, max(e.e_date) as mostRecentlyWonElectionDate, e.id as mostRecentlyWonElectionId
 -- from winning_party w, election e 
 -- where w.election_id = e.id 
 -- group by w.party_id, e.id; 
