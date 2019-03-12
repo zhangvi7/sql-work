a1dbf497a00d6680b5bc8949ac93f68d97a90912
Victor <victorzhang1997@hotmail.com>
2019-03-12 16:54:57 -0400

    x

--- q2.sql
+++ q2.sql
@@ -28,16 +28,21 @@ from election_result e, winning_partys w, party p
 where e.election_id = w.election_id and e.votes = w.max_votes and 
       e.party_id = p.id;
 --2) Using all winning parties in election, find number of wins per party
-DROP VIEW IF EXISTS wins_per_party_no_country CASCADE;
-create view wins_per_party_no_country as
-select w.party_id, count(p.country_id) as party_wins
-from winning_party w right join party p on w.party_id = p.id
-group by w.party_id; 
+-- DROP VIEW IF EXISTS wins_per_party_no_country CASCADE;
+-- create view wins_per_party_no_country as
+-- select w.party_id, count(p.country_id) as party_wins
+-- from winning_party w right join party p on w.party_id = p.id
+-- group by w.party_id; 
 
+-- DROP VIEW IF EXISTS wins_per_party CASCADE;
+-- CREATE VIEW wins_per_party AS
+-- SELECT w.party_id, p.country_id, w.party_wins
+-- FROM wins_per_party_no_country w LEFT JOIN party p ON p.id= w.party_id;
 DROP VIEW IF EXISTS wins_per_party CASCADE;
 CREATE VIEW wins_per_party AS
-SELECT w.party_id, p.country_id, w.party_wins
-FROM wins_per_party_no_country w LEFT JOIN party p ON p.id= w.party_id;
+SELECT num.party_id, party.country_id, num.num_of_winning
+FROM(SELECT winning_party.party_id , count(party.country_id) AS num_of_winning 
+FROM winning_party  RIGHT JOIN party ON winning_party.party_id = party.id GROUP BY party_id) num  LEFT JOIN party ON party.id= num.party_id;
 
 -- 3)average number of winning elections of parties of the same country
 
@@ -47,68 +52,72 @@ select p.country_id, (sum(w.party_wins) / count(w.party_id)) as country_avg_win
 from wins_per_party w right join party p on w.party_id = p.id
 group by p.country_id;
 
+select p.country_id, (sum(w.party_wins) / count(w.party_id)) as country_avg_win
+from wins_per_party w right join party p on w.party_id = p.id
+group by p.country_id;
+
 --4) Find parties that won more than 3 x average win per country 
-DROP VIEW IF EXISTS won_more_3x CASCADE;
-create view won_more_3x as
-select w.country_id, w.party_wins, w.party_id
-from wins_per_party w, avg_wins_country a
-where w.country_id = a.country_id and w.party_wins > (3 * a.country_avg_win);
+-- DROP VIEW IF EXISTS won_more_3x CASCADE;
+-- create view won_more_3x as
+-- select w.country_id, w.party_wins, w.party_id
+-- from wins_per_party w, avg_wins_country a
+-- where w.country_id = a.country_id and w.party_wins > (3 * a.country_avg_win);
 
-create view with_country_name as
-select c.name as countryName, w.party_wins , w.party_id
-from won_more_3x w, country c
-where w.country_id = c.id;
+-- create view with_country_name as
+-- select c.name as countryName, w.party_wins , w.party_id
+-- from won_more_3x w, country c
+-- where w.country_id = c.id;
 
-create view with_party_name as
-select w.countryName, p.name as partyName, w.party_wins as wonElections, w.party_id
-from with_country_name w, party p
-where w.party_id = p.id;
+-- create view with_party_name as
+-- select w.countryName, p.name as partyName, w.party_wins as wonElections, w.party_id
+-- from with_country_name w, party p
+-- where w.party_id = p.id;
 
-create view with_party_family as
-select w.countryName, w.partyName, p.family as familyName, w.wonElections, w.party_id
-from with_party_name w right join party_family p on 
-w.party_id = p.party_id;
+-- create view with_party_family as
+-- select w.countryName, w.partyName, p.family as familyName, w.wonElections, w.party_id
+-- from with_party_name w right join party_family p on 
+-- w.party_id = p.party_id;
 
-DROP VIEW IF EXISTS find_election CASCADE;
-create view find_election as
-select w.party_id, e.election_id
-from with_party_family w left join election_result e 
-on w.party_id = e.party_id;
+-- DROP VIEW IF EXISTS find_election CASCADE;
+-- create view find_election as
+-- select w.party_id, e.election_id
+-- from with_party_family w left join election_result e 
+-- on w.party_id = e.party_id;
 
-select w.party_id, e.election_id
-from with_party_family w left join election_result e 
-on w.party_id = e.party_id;
+-- select w.party_id, e.election_id
+-- from with_party_family w left join election_result e 
+-- on w.party_id = e.party_id;
 
-select w.party_id, e.election_id
-from with_party_family w right join election_result e 
-on w.party_id = e.party_id;
-
-
-DROP VIEW IF EXISTS most_recent_date CASCADE;
-create view most_recent_date as
-select f.party_id, MAX(e.e_date) AS max_date
-from find_election f left join election e 
-on f.election_id = e.id
-group by f.party_id;
-
-select f.party_id, MAX(e.e_date) AS max_date
-from find_election f left join election e 
-on f.election_id = e.id
-group by f.party_id;
-
-select f.party_id, MAX(e.e_date) AS max_date
-from find_election f right join election e 
-on f.election_id = e.id
-group by f.party_id;
-
-create view both as 
-select f.party_id, f.election_id, m.max_date
-from find_election f, most_recent_date m;
+-- select w.party_id, e.election_id
+-- from with_party_family w right join election_result e 
+-- on w.party_id = e.party_id;
 
 
--- DROP VIEW IF EXISTS ans CASCADE;
--- create view ans as
---insert into q3
+-- DROP VIEW IF EXISTS most_recent_date CASCADE;
+-- create view most_recent_date as
+-- select f.party_id, MAX(e.e_date) AS max_date
+-- from find_election f left join election e 
+-- on f.election_id = e.id
+-- group by f.party_id;
+
+-- select f.party_id, MAX(e.e_date) AS max_date
+-- from find_election f left join election e 
+-- on f.election_id = e.id
+-- group by f.party_id;
+
+-- select f.party_id, MAX(e.e_date) AS max_date
+-- from find_election f right join election e 
+-- on f.election_id = e.id
+-- group by f.party_id;
+
+-- create view both as 
+-- select f.party_id, f.election_id, m.max_date
+-- from find_election f, most_recent_date m;
+
+
+-- -- DROP VIEW IF EXISTS ans CASCADE;
+-- -- create view ans as
+-- --insert into q3
 -- select w.countryName, w.partyName,w.familyName, w.wonElections, b.mostRecentlyWonElectionId, 
 -- EXTRACT(year FROM b.mostRecentlyWonElectionDate) AS mostRecentlyWonElectionYear 
 -- from both b, with_party_family w;
