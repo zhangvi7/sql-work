6a6d7d375a4f2fe2cc96791b5fb4d9cb7d9ff185
Victor <victorzhang1997@hotmail.com>
2019-03-12 16:00:02 -0400

    x

--- q2.sql
+++ q2.sql
@@ -11,32 +11,32 @@ create table q3(
 ); 
 
 --Find the number of vote that win that election for each election
-drop view if exists winner_vote cascade
+drop view if exists winner_vote cascade;
 CREATE VIEW winner_vote AS 
 SELECT election_id,max(votes)AS max_vote FROM election_result GROUP BY election_id;
 
  --Find the party that wins the election for each election
- drop view if exists winner cascade
+ drop view if exists winner cascade;
 CREATE VIEW winner AS
 SELECT party.id AS party_id, party.country_id, election_result.election_id
 FROM (election_result NATURAL JOIN winner_vote )JOIN party ON party.id = election_result.party_id
 WHERE winner_vote.max_vote = election_result.votes ;
 
 --Find the number of win for each party. For the party that does not win, set 0
-drop view if exists num_win cascade
+drop view if exists num_win cascade;
 CREATE VIEW num_win AS
 SELECT num.party_id, party.country_id, num.num_of_winning
 FROM(SELECT winner.party_id , count(party.country_id) AS num_of_winning 
 FROM winner  RIGHT JOIN party ON winner.party_id = party.id GROUP BY party_id) num  LEFT JOIN party ON party.id= num.party_id;
 
 --Find the average number of winning elections of each country
-drop view if exists country_avg_win cascade
+drop view if exists country_avg_win cascade;
 CREATE VIEW country_avg_win AS
 SELECT party.country_id, (sum(num_win.num_of_winning)/count(party.id) )AS average 
 FROM num_win RIGHT JOIN party ON num_win.party_id = party.id GROUP BY party.country_id ;
 
 --Find the party that that have won three times the average number of winning elections of parties of the same country
-drop view if exists answer_party cascade
+drop view if exists answer_party cascade;
 CREATE VIEW answer_party AS
 SELECT n.party_id ,c.country_id FROM num_win n JOIN country_avg_win c ON n.country_id = c.country_id 
 WHERE 3*(c.average) < n.num_of_winning ;
