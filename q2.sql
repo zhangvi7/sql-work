21dd482253b47c4506225d2f9ed0c4c0e391fed1
Victor <victorzhang1997@hotmail.com>
2019-03-12 20:25:03 -0400

    x≈y

--- q2.sql
+++ q2.sql
@@ -88,8 +88,8 @@ group by w.party_id;
 
 CREATE VIEW find_election_id AS
 SELECT f.party_id, w.election_id, f.max_date
-FROM (find_election_date f join winning_party w ON f.party_id = w.party_id);    
--- JOIN election ON election.id = winner.election_id AND cast                 (recent.mostRecentlyWonElectionDate AS DATE) = election.e_date;
+FROM (find_election_date f join winning_party w ON f.party_id = w.party_id)   
+JOIN election ON election.id = w.election_id AND cast (f.max_date AS DATE) = election.e_date;
 
 -- DROP VIEW IF EXISTS ans CASCADE;
 -- create view ans as
