ae65f66ec476c27ecd994d1c5caa9027e0dfa2f5
Victor <victorzhang1997@hotmail.com>
2019-03-13 19:55:51 -0400

    x

--- q5.sql
+++ q5.sql
@@ -11,13 +11,13 @@ create view election_full as
 select e.id, e.country_id, extract(YEAR from e_date) as year, e.electorate, e.votes_cast
 from election e;
 
-DROP VIEW IF EXISTS  ratios CASCADE;
-create view ratios as
-select e.country_id, e.year, avg(votes_cast::numeric / electorate::numeric) as ratio
-from election_full e
-where e.year >= 2001 and e.year <= 2016
-group by e.year, e.country_id;
 
+-- Group by each country and year
+CREATE VIEW participation_ratio AS 
+SELECT EXTRACT(year FROM e_date) AS year, country_id, AVG(votes_cast::numeric / electorate::numeric) AS ratio
+FROM election_full
+WHERE e_date > '2001-01-01' AND e_date < '2016-12-31'
+GROUP BY year, country_id;
 
 -- countries whose average election participation ratios during
 -- this period are monotonically non-decreasing (meaning that for Year Y and Year W, where at least
@@ -25,37 +25,44 @@ group by e.year, e.country_id;
 -- ≤ average participation in Year W)
 
 --1) tuples only for countries with at least 1 election, 2) must be non-decreasing ratio over years
--- create view valid_countries as
+
 -- SELECT ID of countries not valid
-create view not_valid_countries as
-select distinct country_id
-FROM ratios r
+CREATE VIEW cid_not_valid AS
+SELECT DISTINCT country_id
+FROM participation_ratio
 WHERE EXISTS (
         SELECT * 
-        FROM ratios r2
+        FROM participation_ratio p
         WHERE 
-                r.year > r2.year AND
-                r.ratio < r2.ratio);
-               
+                participation_ratio.year > p.year AND
+                participation_ratio.ratio < p.ratio);
+                
+SELECT DISTINCT country_id
+FROM participation_ratio
+WHERE EXISTS (
+        SELECT * 
+        FROM participation_ratio p
+        WHERE 
+                participation_ratio.year > p.year AND
+                participation_ratio.ratio < p.ratio);
 
-create view valid_countries as
-select c.id
-from country c
-where not exists (
-    SELECT *
-    FROM not_valid_countries n
-    WHERE n.country_id = c.id
+-- SELECT ID of countries that are valid
+CREATE VIEW cid_valid AS
+SELECT id
+FROM country
+WHERE NOT EXISTS (
+        SELECT * 
+        FROM cid_not_valid
+        WHERE country.id = cid_not_valid.country_id
 );
 
-select c.id
-from country c
-where not exists (
-    SELECT *
-    FROM not_valid_countries n
-    WHERE n.country_id = c.id
+SELECT id
+FROM country
+WHERE NOT EXISTS (
+        SELECT * 
+        FROM cid_not_valid
+        WHERE country.id = cid_not_valid.country_id
 );
-
-
 
 
 
