2ec372ec3e109f81c4aed52622e05aad70260355
Victor <victorzhang1997@hotmail.com>
2019-03-13 19:57:56 -0400

    d

--- q5.sql
+++ q5.sql
@@ -1,11 +1,22 @@
-SET SEARCH_PATH TO parlgov;
-drop table if exists q5 cascade;
+-- Participate
 
-create table q5 (
+SET SEARCH_PATH TO parlgov;
+drop table if exists q3 cascade;
+
+-- You must not change this table definition.
+
+create table q3(
         countryName varchar(50),
         year int,
         participationRatio real
 );
+
+-- You may find it convenient to do this for each of the views
+-- that define your intermediate steps.  (But give them better names!)
+DROP VIEW IF EXISTS intermediate_step CASCADE;
+
+-- Define views for your intermediate steps here.
+
 -- If votes_cast empty, speculate from sum of election results
 CREATE VIEW election_full AS 
 SELECT election.id, election.country_id, election.e_date, electorate,
@@ -43,15 +54,6 @@ WHERE EXISTS (
         WHERE 
                 participation_ratio.year > p.year AND
                 participation_ratio.ratio < p.ratio);
-                
-SELECT DISTINCT country_id
-FROM participation_ratio
-WHERE EXISTS (
-        SELECT * 
-        FROM participation_ratio p
-        WHERE 
-                participation_ratio.year > p.year AND
-                participation_ratio.ratio < p.ratio);
 
 -- SELECT ID of countries that are valid
 CREATE VIEW cid_valid AS
@@ -63,14 +65,12 @@ WHERE NOT EXISTS (
         WHERE country.id = cid_not_valid.country_id
 );
 
-SELECT id
-FROM country
-WHERE NOT EXISTS (
-        SELECT * 
-        FROM cid_not_valid
-        WHERE country.id = cid_not_valid.country_id
-);
 
-
-
-     
+-- the answer to the query 
+insert into q3 
+SELECT country.name AS countryName, 
+        participation_ratio.year AS year, 
+        participation_ratio.ratio AS participationRatio
+FROM participation_ratio, country, cid_valid
+WHERE participation_ratio.country_id = country.id AND 
+        participation_ratio.country_id = cid_valid.id; 
