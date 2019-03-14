c31daa36f2d7120c98d343fcef9e609e6c188848
Victor <victorzhang1997@hotmail.com>
2019-03-13 22:38:46 -0400

    x

--- q5.sql
+++ q5.sql
@@ -6,28 +6,20 @@ create table q5 (
         year int,
         participationRatio real
 );
-DROP VIEW IF EXISTS  election_full CASCADE;
-create view election_full as 
+
+DROP VIEW IF EXISTS elections CASCADE;
+create view elections as 
 select e.id, e.country_id, extract(YEAR from e_date) as year, e.electorate, e.votes_cast
 from election e;
 
-DROP VIEW IF EXISTS  ratios CASCADE;
+DROP VIEW IF EXISTS ratios CASCADE;
 create view ratios as
-select e.country_id, e.year, avg(votes_cast::numeric / electorate::numeric) as ratio
-from election_full e
+select e.country_id, e.year, avg(votes_cast / electorate) as ratio
+from elections e
 where e.year >= 2001 and e.year <= 2016
 group by e.year, e.country_id;
 
-
-
--- countries whose average election participation ratios during
--- this period are monotonically non-decreasing (meaning that for Year Y and Year W, where at least
--- one election has happened in each of them, if Y < W, then the average participation in Year Y is
--- ≤ average participation in Year W)
-
---1) tuples only for countries with at least 1 election, 2) must be non-decreasing ratio over years
--- create view valid_countries as
--- SELECT ID of countries not valid
+DROP VIEW IF EXISTS not_valid_countries CASCADE;
 create view not_valid_countries as
 select distinct country_id
 FROM ratios r
@@ -39,18 +31,15 @@ WHERE EXISTS (
                 r.ratio < r2.ratio and
                 r.country_id = r2.country_id);
 
-               
+DROP VIEW IF EXISTS valid_countries CASCADE;              
 create view valid_countries as
 select *
 from ratios r
 where not exists (
     SELECT *
     FROM not_valid_countries n
-    WHERE r.country_id = n.country_id
-);
+    WHERE r.country_id = n.country_id);
 
-
--- the answer to the query 
 insert into q5 
 SELECT c.name AS countryName, 
        v.year AS year, 
@@ -59,12 +48,6 @@ FROM valid_countries v, country c
 WHERE v.country_id = c.id;
 
 
-
-SELECT c.name AS countryName, 
-       v.year AS year, 
-        v.ratio AS participationRatio
-FROM valid_countries v, country c
-WHERE v.country_id = c.id;
 
 
      
